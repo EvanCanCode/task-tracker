6248ee97c24529d95bc825a86fef75b05398e4fd
Evanescence P. Naad <EvanCanCode.08@gmail.com>
2024-08-19 09:04:38 +0400

    08-19-2024 09:04 PM

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,6 +5,19 @@ import {VscCheck} from "react-icons/vsc";
 
 function App() {
   const [isCompleteScreen, setIsCompleteScreen] = useState(false);
+  const [allTasks, setTasks] = useState([]);
+  const [newTitle, setNewTitle] = useState("");
+  const [newDescription, setNewDescription] = useState("");
+  const handdleAddTask = () => {
+    let newTaskItem = {
+      title:newTitle,
+      description:newDescription
+    }
+
+    let updatedTaskArr = [...allTasks];
+    updatedTaskArr.push(newTaskItem);
+    setTasks(updatedTaskArr);
+  }
   
   return (
     <div className="App">
@@ -13,16 +26,16 @@ function App() {
         <div className="task-input">
           <div className='task-input-item'>
             <label>Task</label>
-            <input type="text" placeholder="What's happening?"/>
+            <input type="text" value={newTitle} onChange={(e) => setNewTitle(e.target.value)} placeholder="What's happening?"/>
           </div>
 
           <div className='task-input-item'>
             <label>Description</label>
-            <input type="text" placeholder="What's happening?"/>
+            <input type="text" value={newDescription} onChange={(e) => setNewDescription(e.target.value)} placeholder="What's happening?"/>
           </div>
 
           <div className='task-input-item'>
-            <button type='button' className='primaryButton'>Add</button>
+            <button type='button' onClick={handdleAddTask} className='primaryButton'>Add</button>
           </div>
         </div>
 
@@ -32,14 +45,19 @@ function App() {
         </div>
 
         <div className="task-list">
-          <div className='task-list-item'>
-            <h3>Task #1</h3>
-            <p>Lorem ipsum dolor sit amet.</p>
-            <div>
-              <VscCircleSlash className='icon'/>
-              <VscCheck className='check-icon'/>
-            </div>
-          </div>
+          {allTasks.map((item, index) => {
+            return(
+              <div className='task-list-item' key={index}>
+                <h3>{item.title}</h3>
+                <p>{item.description}</p>
+                <div>
+                  <VscCircleSlash className='icon'/>
+                  <VscCheck className='check-icon'/>
+                </div>
+              </div>
+            )
+            }
+          )}
         </div>
       </div>
     </div>
